a32665d7efad433ca8ca62775a5905c4f23445ba
j.morgenroth <j.morgenroth@53181e23-2ae4-8a3e-eeed-a9433cda2575>
2011-02-17 10:06:42 +0000

    New mobility model for TheONE movement files.

--- master/src/mobility/mobilitymodel.py
+++ master/src/mobility/mobilitymodel.py
@@ -4,6 +4,8 @@ __date__ ="$29.04.2010 14:05:02$"
 
 import randomwalk
 import logplay.theone
+import movement.theone
+from mobilitynode import MobilityNode
 
 class MobilityModel:
 
@@ -16,6 +18,14 @@ class MobilityModel:
             self.model=logplay.theone.TheONEReader(setup.ctrl.getNodes(), setup.config)
         elif self.mobilitymodule == "static":
             self.model=logplay.static.StaticConnections(setup.ctrl.getNodes(), setup.config)
+        elif self.mobilitymodule == "movement":
+            #Creating mobility nodes
+            mob_nodes = []
+            for node in setup.ctrl.getNodes():
+                cn = MobilityNode(node)
+                mob_nodes.append(cn)
+            
+            self.model = movement.theone.Movement(mob_nodes, setup.setupdir, setup.config)
         else:
             print("Mobility: Unknown model "+str(self.mobilitymodule))
             self.start=self.nomodule
---
+++ master/src/mobility/movement/theone.py
@@ -0,0 +1,131 @@
+'''
+Created on 17.02.2011
+
+@author: morgenro
+'''
+
+import threading
+import os
+import time
+import math
+
+class Movement(object):
+    
+    def __init__(self, nodes, setupdir, config):
+        self.filename = os.path.join(setupdir, config.get("movement", "file"))
+        self.reader = None
+        self.nodes = nodes
+
+        # by default all nodes are disabled
+        for n in self.nodes:
+            n.disabled = True
+            n.range = float(config.get("movement", "range"))
+    
+    def start(self):
+        self.reader = MovementReader(self.filename, self.nodes)
+        self.reader.start()
+
+    def stop(self):
+        self.reader.stop()
+    
+    
+class MovementReader(threading.Thread):
+    
+    def __init__(self, filename, nodes):
+        threading.Thread.__init__(self)
+        self.daemon=True
+        self.filename = filename
+        self.eof = False
+        self.file = None
+        self.buffer = None
+        self.nodes = nodes
+        
+    def run(self):
+        print("TheONE movement reader starting")
+        self.running = True
+        self.file = open(self.filename, 'r')
+        
+        ''' read the first line. it contains the min/max values for the area '''
+        self.file.readline()
+        
+        ''' but we start with the second line '''
+        self.readdata()
+        
+        ''' store the current time for comparison in the next round '''
+        prevtime = self.currenttime
+        
+        ''' set disabled flag, we want to skip the movement trace until at least one node is not disabled '''
+        disabled = True
+        
+        while not self.eof:
+            ''' get the current node '''
+            n = self.nodes[self.nodeid]
+            
+            ''' disable a node if it is out of range '''
+            if self.x > 0 and self.y > 0:
+                n.disabled = False
+                disabled = False
+                n.x = self.x
+                n.y = self.y
+                n.updatePosition()
+            else:
+                n.disabled = True
+                
+            if prevtime != self.currenttime:
+                if not disabled:
+                    time.sleep(self.currenttime - prevtime)
+                print("Timestamp: " + str(self.currenttime))
+                prevtime = self.currenttime
+                
+                ''' check all node connections '''
+                self.updateConnections()
+            
+            ''' read the next line '''
+            self.readdata()
+        
+        print("TheONE movement finished")
+        
+    def updateConnections(self):
+        #print("Update Connections "+str(len(self.nodes)))
+        for me in xrange(0,len(self.nodes)-1):
+            for him in xrange(me+1,len(self.nodes)):
+                if self.nodes[me].disabled:
+                    self.nodes[me].disconnectFrom(self.nodes[him])
+                    continue
+                
+                #print("Compare "+self.nodes[me].name+" with "+self.nodes[him].name)
+                if self.nodes[me].distanceTo(self.nodes[him]) < self.nodes[me].range:
+                    #print("Connect "+str(self.nodes[me].name)+" <-> "+str(self.nodes[him].name))
+                    self.nodes[me].connectTo(self.nodes[him])
+                else:
+                    #print("Disconnect "+str(self.nodes[me].name)+" -/- "+str(self.nodes[him].name))
+                    self.nodes[me].disconnectFrom(self.nodes[him])
+        
+    def readdata(self):
+        buffer = self.file.readline().strip('\n')
+        
+        if (buffer == ''):
+            self.eof = True
+        else:
+            ''' read the current timestamp '''
+            (currenttime, buffer) = buffer.split(" ", 1)
+            
+            ''' read the node id '''
+            (nodeid, buffer) = buffer.split(" ", 1)
+            
+            ''' read the x coordinate '''
+            (x, buffer) = buffer.split(" ", 1)
+            
+            ''' read the y coordinate '''
+            y = buffer
+            
+            ''' translate string values to numeric values '''
+            self.currenttime = int(currenttime)
+            self.nodeid = int(nodeid)
+            self.x = float(x)
+            self.y = float(y)
+            
+    def stop(self):
+        print("TheONE movement reader stopping")
+        self.running = False
+        
